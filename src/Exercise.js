10058d59de01a310c2633c40a21002a11e30b486
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-12-04 15:00:41 -0500

    exercise and workout .js

---
+++ src/Exercise.js
@@ -0,0 +1,12 @@
+class Exercise {
+  constructor(id, name, description, muscle_group, image, video) {
+    this.id = id,
+    this.name = name,
+    this.description = description,
+    this.muscle_group = muscle_group,
+    this.image = image,
+    this.video = video
+  }
+
+  
+}
