9a5c20f3d7b9d92e59c29bb3485993a477d0f6f5
Drewisatlas <drewisatlas@gmail.com>
2018-12-04 14:59:00 -0500

    Barebones Html added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # build-a-bod-frontend
+
+make a form at the top that accepts the name of the workout.
+
+day of the week
+
+Muscle group selector
+
+duration
---
+++ src/Workout.js
@@ -0,0 +1,10 @@
+class Workout {
+  constructor(name, day, muscleGroup, duration) {
+    this.name = name
+    this.day = day
+    this.muscleGroup = muscleGroup
+    this.duration = duration
+    this.exercises = []
+  }
+  
+}
