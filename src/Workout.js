b2ab43b68c02e00e0efb0543a8b3e2a2a5b41cb2
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-12-06 10:56:15 -0500

    some css styling

--- src/Workout.js
+++ src/Workout.js
@@ -24,30 +24,32 @@ class Workout {
 
     let name = document.createElement('h3')
     name.innerText = `${this.name}`
-    name.classList.add('dropzone')
+    // name.classList.add('dropzone')
     name.dataset.workoutId = `${this.id}`
 
     let day = document.createElement('p')
     day.innerText = `${this.day}`
-    day.classList.add('dropzone')
+    // day.classList.add('dropzone')
     day.dataset.workoutId = `${this.id}`
 
     let muscleGroup = document.createElement('p')
     muscleGroup.innerText = `Muscle Group: ${this.muscle_group}`
-    muscleGroup.classList.add('dropzone')
+    // muscleGroup.classList.add('dropzone')
     muscleGroup.dataset.workoutId = `${this.id}`
 
     let duration = document.createElement('p')
     duration.innerText = `${this.duration} minutes`
-    duration.classList.add('dropzone')
+    // duration.classList.add('dropzone')
     duration.dataset.workoutId = `${this.id}`
 
     let exercises = document.createElement('div')
+    exercises.className = "exercises-div"
     exercises.id = `exercises-${this.id}`
-    exercises.classList.add('dropzone')
+    // exercises.classList.add('dropzone')
     exercises.dataset.workoutId = `${this.id}`
 
     let deleteButton = document.createElement('button')
+    deleteButton.className = "delete-btn"
     deleteButton.id = `delete-workout-${this.id}`
     deleteButton.innerText = 'Delete Workout'
     deleteButton.dataset.workoutId = `${this.id}`
